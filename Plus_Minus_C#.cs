762b168c38640c70b2f3ae3fc560cd74d61e6107
Marta <118316713+martapolii@users.noreply.github.com>
2024-10-16 12:47:46 -0400

    Update Plus_Minus_C#.cs
    Comment out question

--- Plus_Minus_C#.cs
+++ Plus_Minus_C#.cs
@@ -1,15 +1,15 @@
-Q: Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+// Q: Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
 
-Input Format
+// Input Format
 
-The first line contains an integer, , the size of the array.
-The second line contains  space-separated integers that describe arr[n].
+// The first line contains an integer, , the size of the array.
+// The second line contains  space-separated integers that describe arr[n].
 
-Output Format
-Print the following  lines, each to 6 decimals:
--proportion of positive values
--proportion of negative values
--proportion of zeros
+// Output Format
+// Print the following  lines, each to 6 decimals:
+// -proportion of positive values
+// -proportion of negative values
+// -proportion of zeros
 
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
